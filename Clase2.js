c80e877f6810136c1558cb23b6015e11ab1922a0
JakelinMishelQuinoMartinez <quinojakelin@gmail.com>
2026-04-23 15:58:03 -0600

    feat: :sparkles: Enunciados de los ejercicios de la clase 2
    Intrucciones para resolver los 20 ejercicios

---
+++ Clase2.js
@@ -0,0 +1,67 @@
+/**01 FUNCIÓN QUE DETERMINA SI UN NÚMERO ES POSITIVO, NEGATIVO O CERO
+ * Crear una función que reciba un número y muestre el resultado*/
+console.log("Ejercicio 1")
+
+/**02 FUNCIÓN QUE DETERMINE SI UN NÚMERO ES PAR O IMPAR */
+console.log("Ejercicio 2")
+
+/**03 FUNCIÓN QUE DETEMINA SI UNA PERSONA ES MAYOR DE EDAD */
+console.log("Ejercicio 3")
+
+/**04 MOSTRAR LOS NÚMEROS DEL 1 AL 10 CON FOR*/
+console.log("Ejercicio 4")
+
+/**05 MOSTRAR LOS NÚMEROS DEL 10 AL 1 CON WHILE */
+console.log("Ejercicio 5")
+
+/**06 FUNCIÓN QUE MUESTRE LA TABLA DE MULTIPLICAR DE UN NÚMERO*/
+console.log("Ejercicio 6")
+
+/**07 SUMAR LOS NÚMEROS DEL 1 AL 100 USANDO UN CICLO */
+console.log("Ejercicio 7")
+
+/**08 FUNCIÓN QUE RECIBA DOS NÚMEROS Y DEVUELVA EL MAYOR */
+console.log("Ejercicio 8")
+
+/**09 CONTAR CUNTAS LETRAS TIENE UNA PALABRA USANDO UN CICLO */
+console.log("Ejercicio 9")
+
+/**10 FUNCIÓN QUE CUENTE CUÁNTAS VOCALES TIENE UNA PALABRA */
+console.log("Ejercicio 10")
+
+/**11 FUNCIÓN QUE RECIBA UN NÚMERO Y MUESTRE TODOS LOS PARES HASTA ESE NÚMERO */
+console.log("Ejercicio 11")
+
+/**12 FUNCIÓN QUE CALCULE EL FACTORIAL DE UN NÚMERO */
+console.log("Ejercicio 12")
+
+/**13 FUNCIÓN QUE RECIBA UNA FRASE Y LA IMPRIMA CARÁCTER POR CARÁCTER */
+console.log("Ejercicio 13")
+
+/**14 FUNCIÓN QUE INVIERTA UNA PALABRA (EJ: "HOLA" => "ALOH") */
+console.log("Ejercicio 14")
+
+/**15 FUNCIÓN QUE DETEMINE SI UNA PALABRA ES PALÍNDORMA */
+console.log("Ejercicio 15")
+
+/**16 FUNCIÓN QUE SUME LOS NÚMEROS PARES DEL 1 AL 50 */
+console.log("Ejercicio 16")
+
+/**17 FUNCIÓN QUE RECIBA UN NÚMERO Y DIGA SI ES PRIMO */
+console.log("Ejercicio 17")
+
+/**18 FUNCIÓN QUE MUESTRE UN PATRÓN DE ASTERISCOS
+ * *
+ * **
+ * ***
+ * ****
+ */
+console.log("Ejercicio 18")
+
+/**19 FUNCIÓN QUE SIMULE UN CAJERO AUTOMÁTICO 
+ * Recibe un saldo y un retiro, valida si puede retirar o no*/
+console.log("Ejercicio 19")
+
+/**20FUNCIÓN QUE SIMULE UN LOGIN
+ * Recibe usuario y contraseña y valida si coinciden con valores defenidos en el programa */
+console.log("Ejercicio 20")
