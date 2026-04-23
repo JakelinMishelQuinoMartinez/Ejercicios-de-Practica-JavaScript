d4b91f085787d41cc7da693979930db3bec0eace
JakelinMishelQuinoMartinez <quinojakelin@gmail.com>
2026-04-23 14:05:15 -0600

    feat: :sparkles: Enunciado de los ejercicios de la clase 1
    Intrucciones para resolver los 20 ejercicios

---
+++ Clase1.js
@@ -0,0 +1,82 @@
+/**01 DATOS PERSONALES
+ * Declarar variables para: nombre, edad, ciudad
+ * Mostrar un mensaje:
+ * Hola, mi nombre es ___, tengo ___ años y vivo en ___ */
+console.log("Ejercicio 1: DATOS PERSONALES")
+
+/**02 SUMA DE DOS NÚMEROS
+ * Declarar dos números y muestra su suma */
+console.log("Ejercicio 2: SUMA DE DOS NÚMEROS")
+
+/**03 RESTA, MULTIPLICACIÓN Y DIVISÓN 
+ * Con los mismos númereos, muestra los 4 resultados matemáticos básicos*/
+console.log("Ejercicio 3: RESTA, MULTIPLICACIÓN, DIVISÓN y POTENCIA")
+
+/**04 PROMEDIO DE 3 NOTAS
+ * Declara tres notas y calculña el promedio */
+console.log("Ejercicio 4: PROMEDIO DE 3 NOTAS")
+
+
+/**05 CONVERSIÓN DE EDAD A MESES
+ * Declara tu edad y calcula cuántos meses has vivido */
+console.log("Ejercicio 5: CONVERSIÓN DE EDAD A MESES")
+
+/**06 CONVERSIÓN DE HORAS A MINUTOS Y SEGUNDOS
+ * Declara una cantidad de horas y conviértala a minutos y segundos */
+console.log("Ejercicio 6: CONVERSIÓN DE HORAS A MINUTOS Y SEGUNDOS")
+
+/**07 PRECIO CON IVA
+ * Declara el precio de un producto y calcula el valor final con IVA (19%)*/
+console.log("Ejercicio 7: PRECIO CON IVA")
+
+/**08 ÁREA DE UN RECTÁNGULO
+ * Declare base y altura y calcula el área */
+console.log("Ejercicio 8: ÁREA DE UN RECTÁNGULO")
+
+/**09 PERÍMETRO DE UN RECTÁNGULO
+ * Con la misma base y altura calcular el perímetro */
+console.log("Ejercicio 9: PERÍMETRO DE UN RECTÁNGULO")
+
+/**10 CONCETRACIÓN DE STRINGS
+ * Declara nombre y apellido en variables separadas y únerlos en un solo string */
+console.log("Ejercicio 10: CONCETRACIÓN DE STRINGS")
+
+/**11 LONGITUD DE UNA PALABRA 
+ * Declara una palabra y muestra cuántos carácteres tiene usando .length*/
+console.log("Ejercicio 11: LONGITUD DE UNA PALABRA ")
+
+/**12 PRIMERA Y ÚLTIMA LETRA
+ * Muestra la primera y la última letra de cada palabra */
+console.log("Ejercicio 12: PRIMERA Y ÚLTIMA LETRA")
+
+/**13 CONVERTIR EL TEXTO A MAYÚSCULAS Y MINÚSCULAS
+ * Declara una frase y muéstrala en mayúsculas y en minúsculas */
+console.log("Ejercicio 13: CONVERTIR EL TEXTO A MAYÚSCULAS Y MINÚSCULAS")
+
+/**14 EXTRAER PARTE DE UN TEXTO
+ * Declarar una frase y extrae solo una palabra usando .slice */
+console.log("Ejercicio 14: EXTRAER PARTE DE UN TEXTO")
+
+/**15 REMPLAZAR PALABRA EN UNA FRASE
+ * REMPLAZAR UNA PALABRA POR OTRA USANDO .REPLACE() */
+console.log("Ejercicio 15: REMPLAZAR PALABRA EN UNA FRASE")
+
+/**16 OPERADORES DE INCREMENTO
+ * Declarar un número y aplícale ++ tres veces mostrando el resultado cada vez */
+console.log("Ejercicio 16: OPERADORES DE INCREMENTO")
+
+/**17 OPERADORES COMPUESTOS
+ * Declara un número y usa +=, -=, *=, /= */
+console.log("Ejercicio 17: OPERADORES COMPUESTOS")
+
+/**18 CONCATENAR NÚMERO Y TEXTO
+ * Delcara un número y un texto y concatenarlos en una frase */
+console.log("Ejercicio 18: CONCATENAR NÚMERO Y TEXTO")
+
+/**19 CALCULAR EL RESIDUO
+ * Declara dos números y muestra el residuo usando % */
+console.log("Ejercicio 19: CALCULAR EL RESIDUO")
+
+/**20 CONSTRUIR UN MENSAJE COMPLEJO
+ * Con varias variables (nombre, edad, hobby, comida favorita), construye un menssaje largo concatenando todo */
+console.log("Ejercicio 20: CONSTRUIR UN MENSAJE COMPLEJO")
