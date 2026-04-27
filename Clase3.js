322d280a7c2c0f0bdc282c570911564dc7507645
JakelinMishelQuinoMartinez <quinojakelin@gmail.com>
2026-04-27 16:24:28 -0600

    feat: :sparkles: Enunciados de los ejercicios de la clase 2
    Intrucciones para resolver los 20 ejercicios

---
+++ Clase3.js
@@ -0,0 +1,62 @@
+/**01 CREAR UN OBJETO ESTUDIANTE
+ * Crear un objeto con nombre, edad y curso. Mostrar un mensaje usando desestructuración*/
+
+/**02 LISTA DE ESTUDIANTES
+ * Crear un arreglo con 5 objetos estudiante. Recorrelo e imprimir los nombres*/
+
+/**03 PROMEDIO DE NOTAS POR ESTUDIANTE
+ * Cada estudiante tiene un arreglo de notas. Calcular el promedio de cada uno*/
+
+/**04 BUSCAR EL ESTUDIANTE MAYOR
+ * Recorrer el arreglo y mostrar el estudiante con mayor edad*/
+
+/**05 FILTRAR ESTUDIANTES APROBADOS 
+ * Mostrar solo los que tengan promedio mayor o igual a 3.0.*/
+
+/**06 DESESTRUCTURAR DENTRO DE UN CICLO
+ * Recorrer el arreglo y desestructurar nombre y edad directamente en el for*/
+
+/**07 CREAR UN SET DE MATERIAS
+ * Agrergar varias materias, algunas repetidas, Mostrar el contenido final del Set*/
+
+/**08 CONVERTIR UN ARREGLO A SET 
+ * Dado un arreglo con números repetidos, convertirlo convertirlo a set para eliminar duplicados*/
+
+/**09 CREAR UN MAP DE PRODUCTOS 
+ * Clave: nombre del producto.
+ * Valor: precio.
+ * Mostrar todos los productos con su precio
+*/
+
+/**10 CALCULAR EL TOTAL DEL CARRITO USANDO MAP
+ * Recorrer el Map y sumar todos los precios*/
+
+/**11 VERIFICAR EXCISTENCIA EN MAP
+ * Pedir un producto y validar si existe en el Map*/
+
+/**12 OBJETO CON MÉTODO
+ * Crear un objeto cuentaBancaria con propiedades y un método para depositar dinero*/
+
+/**13 LISTA DE OBJETOS PRODUCTOS
+ * Cada producto tiene nombre, precio y categoría. Mostrar solo los de una categoría específica*/
+
+/**14 ORDENAR PRODUCTOS POR PRECIO
+ * Ordenar el arreglo de productos de menor a mayo precio*/
+
+/**15 DESTRUCTURACIÓN ANIDADA
+ * Crear un objeto que tenga otro objeto dentro (dirección) y extraer los datos con desestructuración*/
+
+/**16 CONTAR ELEMENTOS ÚNICOS CON SET
+ * Dado un arreglo de nombres repetidos, contar cuántos nombre únicos hay*/
+
+/**17 CREAR UN MAP A PARTIR DE UN ARREGLO
+ * Dado un arreglo de objetos productos, pasarlo a un Map */
+
+/**18 ELIMINAR ELEMENTOS DE UN SET
+ * Eliminar un elemento y mostrar el resultado*/
+
+/**19 RECORRER UN MKAP CON DESESTRUCTURACIÓN
+ * MOstrar clave y valor usando desestructuración en el for */
+
+/**20 MINI SISTEMA DE BIBLIOTECA
+ * Crear un arreglo de libors (objeto de título, autor y estado: prestado/disponible). Mostrar solo los disponibles */
